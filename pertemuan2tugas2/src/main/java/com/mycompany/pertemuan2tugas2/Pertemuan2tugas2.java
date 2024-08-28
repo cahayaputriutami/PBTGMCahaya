be970dae7870903905a6bf01dc9d11071a8161a6
cahayaaa <146090366+cahayaputriutami@users.noreply.github.com>
2024-08-28 14:30:34 +0700

    pertemuan 2 tugas 2 VARIABEL PADA JAVA

---
+++ pertemuan2tugas2/src/main/java/com/mycompany/pertemuan2tugas2/Pertemuan2tugas2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan2tugas2;
+
+/**
+ *
+ * @author Windows 11
+ */
+public class Pertemuan2tugas2 {
+
+    public static void main(String[] args) {
+        int a=50;
+float b=a;
+System.out.println(a);
+System.out.println(b);
+    }
+}
