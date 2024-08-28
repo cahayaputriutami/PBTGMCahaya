216b4313e94d500d81349d1d520a99dfb45e8594
cahayaaa <146090366+cahayaputriutami@users.noreply.github.com>
2024-08-28 08:11:06 +0700

    pertemuan 9 tugas 3 MENGONTROL ALIRAN PROGRAM PERULANGAN WHILE, DO WHILE

---
+++ pertemuan9tugas3/src/main/java/com/mycompany/pertemuan9tugas3/Pertemuan9tugas3.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan9tugas3;
+
+/**
+ *
+ * @author pc
+ */
+public class Pertemuan9tugas3 {
+
+    public static void main(String[] args) {
+         int i = 1;
+        while (i < 6) {
+            System.out.println(i);
+            i++; // Menambah nilai i agar loop tidak berjalan selamanya
+        }
+    }
+}
