bb434d1c1a2869688517eb3466431aa6a8a5d1fe
cahayaaa <146090366+cahayaputriutami@users.noreply.github.com>
2024-09-07 23:06:06 +0700

    pertemuan 10 tugas 1 KONSEP PEMROGRAMAN BERORIENTASI OBJEK CLASS. OBJECT. ATTRIBUTE. METHOD

---
+++ pertemuan10tugas1/src/main/java/com/mycompany/pertemuan10tugas1/Pertemuan10tugas1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan10tugas1;
+
+/**
+ *
+ * @author Windows 11
+ */
+public class Pertemuan10tugas1 {
+
+    public static void main(String[] args) {
+        Aritmatika aritmatika = new Aritmatika();
+        
+        System.out.println("Pengurangan: " + aritmatika.pengurangan(10, 5));
+        System.out.println("Perkalian: " + aritmatika.perkalian(10, 5));
+        System.out.println("Pembagian: " + aritmatika.pembagian(10, 2));
+        System.out.println("Pangkat: " + aritmatika.pangkat(2, 3));       
+    }
+}
